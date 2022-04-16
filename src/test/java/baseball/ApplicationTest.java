b52ebbbc26e349c92c2e3e70fd528e188059ffe5
DongGyuJang <cdgu1129@knou.ac.kr>
2022-04-17 03:05:12 +0900

    test: gamestart, gameover 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,16 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.jupiter.api.Test;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -28,6 +32,44 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 게임시작_에러() {
+    	assertSimpleTest(() ->
+	    	assertThatThrownBy(() -> {
+	            System.setIn(strToByteArrayInputStream("1234"));
+	            Application.gamestart(new BaseballComputer());
+	    	}).isInstanceOf(IllegalArgumentException.class)
+		);
+    }
+
+    @Test
+    void 게임오버_테스트1() {
+        System.setIn(strToByteArrayInputStream("1"));
+        assertThat(Application.gameover()).isInstanceOf(BaseballComputer.class);
+    }
+
+    @Test
+    void 게임오버_테스트2() {
+        System.setIn(strToByteArrayInputStream("2"));
+        assertThat(Application.gameover()).isEqualTo(null);
+    }
+    
+    @Test
+    void 게임오버_테스트3() {
+    	assertSimpleTest(() ->
+	    	assertThatThrownBy(() -> {
+	            System.setIn(strToByteArrayInputStream("3"));
+	            Application.gameover();
+	    	}).isInstanceOf(IllegalArgumentException.class)
+		);
+    }
+    
+    private ByteArrayInputStream strToByteArrayInputStream(String str) {
+        final byte[] buf = str.getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    	return new ByteArrayInputStream(buf);
+    }
+    
     @Override
     public void runMain() {
         Application.main(new String[]{});
