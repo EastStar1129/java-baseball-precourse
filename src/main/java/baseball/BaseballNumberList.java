7ecc775a8fb4cc147c775b962f0f4324b907c824
DongGyuJang <cdgu1129@knou.ac.kr>
2022-04-16 10:59:25 +0900

    feat: 작성한 테스트코드 통과 로직구현

--- src/main/java/baseball/BaseballNumberList.java
+++ src/main/java/baseball/BaseballNumberList.java
@@ -1,7 +1,51 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BaseballNumberList {
-	public BaseballNumberList(String number) {
+	
+	public BaseballNumberList(String numbers) {
+		validate(numbers);
+	}
+	
+	private void validate(String numbers) {
+		validateCount(numbers);
+		validateOverlap(numbers);
+		validateRange(numbers);
+	}
+
+	private void validateCount(String numbers) {
+		if(numbers.length() != 3) {
+            throw new IllegalArgumentException("BaseballNumberList count error");
+		}
+	}
+	
+	private void validateOverlap(String numbers) {
+		Set<Character> numberSet = StringToSet(numbers);
 		
+		if(numberSet.size() != 3) {
+            throw new IllegalArgumentException("BaseballNumberList overlap error");
+		}
+	}
+	
+	private Set<Character> StringToSet(String numbers){
+		Set<Character> numberSet = new HashSet<>();
+		for(char number : numbers.toCharArray()) {
+			numberSet.add(number);
+		}
+		return numberSet;
+	}
+	
+	private void validateRange(String numbers) {
+		for(char number : numbers.toCharArray()) {
+			checkRange(number);
+		}
+	}
+	
+	private void checkRange(char number) {
+		if(number < '1' || number > '9') {
+            throw new IllegalArgumentException("BaseballNumber Range(1~9) error");
+		}
 	}
 }
