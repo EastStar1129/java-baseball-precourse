1748c27e705a01346b044de6c29ab9c0b79813dc
DongGyuJang <cdgu1129@knou.ac.kr>
2022-04-17 03:10:26 +0900

    feat: 테스트 통과로직, view 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+	private static final String GAMEOVER_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\r\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	gamestart(new BaseballComputer());
+    }
+    
+    public static void gamestart(BaseballComputer bc) {
+    	BaseballState output = compareInput(bc);
+    	System.out.println(output.toString());
+    	if(output.getStrike() == 3) {
+    		bc = gameover();
+    	}
+    	if(bc != null) {
+    		gamestart(bc);
+    	}
+    }
+    
+    public static BaseballState compareInput(BaseballComputer bc) {
+    	System.out.print("숫자를 입력해주세요 : ");
+    	String input = Console.readLine();
+    	return bc.compare(input);
+    }
+    
+    public static BaseballComputer gameover() {
+		System.out.println(GAMEOVER_MSG);
+    	String input = Console.readLine();
+    	if(input.equals("1")) {
+    		return new BaseballComputer();
+    	}
+    	if(input.equals("2")) {
+    		return null;
+    	}
+    	throw new IllegalArgumentException("applcation restart input error, input awalys 1,2");
     }
 }
